780363b3966cb05bc5a97b1329cf6b608aec0b22
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-03-07 12:27:17 +0600

    testing for inbox contract

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,29 +2,49 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('..compile');
 
-class Car {
-    park() {
-        return 'stopped';
-    }
+let accounts;
+let inbox;
 
-    drive() {
-        return 'vroom'
-    }
-}
-
-let car;
-
-beforeEach(() => {
-    car = new Car();
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    // use one of those accounts to deploy 
+    // the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' })
 })
 
-describe('Car', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
     });
+});
 
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
-    });
-})
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vroom'
+//     }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//     car = new Car();
+// })
+
+// describe('Car', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// })
