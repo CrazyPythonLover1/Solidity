8fba41c10a1730dffee2befac956c27f3f03fd08
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-03-07 08:12:05 +0600

    compile.js file

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,17 +1,19 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.7.4;
 
 contract Inbox {
     string public message;
     
-    function Inbox(string initialMessage) public {
+    function InboxFunc(string memory initialMessage) public {
         message = initialMessage;
     }
     
-    function setMessage(string newMessage) public {
+    function setMessage(string memory newMessage) public {
         message = newMessage;
     }
     
-    function getMessage() public view returns (string) {
+    function getMessage() public view returns (string memory) {
         return message;
     }
 }
