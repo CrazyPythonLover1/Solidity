79ed047e99f14a87845d7c471b95ba11aae94618
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-03-13 21:54:06 +0600

    add mapping into compaign

--- inbox/contracts/campaign.sol
+++ inbox/contracts/campaign.sol
@@ -7,12 +7,15 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
     
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    // address[] public approvers;
+    mapping(address => bool) public approvers;
     
     modifier restricted() {
         require(msg.sender == manager);
@@ -27,20 +30,40 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimumContribution);
         
-        approvers.push(msg.sender); 
+        // approvers.push(msg.sender); // it is not a array now
+        approvers[msg.sender] = true;
     }
-    
+      
     function createRequest(string description, uint value, address recipient) 
     public restricted {
-        Request newRequest = Request({
+        Request memory    newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
         });
         
         requests.push(newRequest);
     }
     
-    
 }
+
+// poor approach for voting system 
+// contract Campaign { 
+//     function approveRequest(Request request) public {
+//         // Make sure person calling this function has donated 
+//         bool isApprover = false;
+//         for (uint i = 0; i< approvers.length; i++) {
+//             if (approvers[i] == msg.sender) {
+//                 isApprover = true;
+//             }
+//         }
+//         require(isApprover);
+        
+//         // Make sure person calling this function hasn't voted before
+//         for (uint i = 0; i < request.approvers.length; i++) {
+//             require(request.approvers[i] != msg.sender);
+//         }
+//     }
+// }
